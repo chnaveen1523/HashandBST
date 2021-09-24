6d1c0788f0536c9a8f732e7bf259ad2c905d9ec8
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-24 22:59:32 +0530

    [Naveen] Added a code

---
+++ BST.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableandBST.cs
+{
+    class BinarySearchTree<T> where T : IComparable<T>
+    {/// <summary>
+     /// UC3 for bst to search 63 of the binary search tree
+     /// </summary>
+        public T RootNode { get; set; }
+        public BinarySearchTree<T> LeftTree { get; set; }
+        public BinarySearchTree<T> RightTree { get; set; }
+        public BinarySearchTree(T RootNode)
+        {
+            this.RootNode = RootNode;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+
+        public static int leftCount = 0, rightCount = 0;
+        bool result = false;
+
+        //method for inserting element in binary search tree
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.RootNode;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                {
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+                    this.LeftTree.Insert(item);
+                }
+            }
+            else
+            {
+                if (this.RightTree == null)
+                {
+                    this.RightTree = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+                    this.RightTree.Insert(item);
+                }
+            }
+        }
+
+        //method to give size of the binary search tree
+        public void GetSize()
+        {
+            Console.WriteLine("The size of the tree is" + " " + (1 + leftCount + rightCount));
+        }
+
+        //method for search node in BST
+        public bool Search(T element, BinarySearchTree<T> node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.RootNode.Equals(element))
+            {
+                Console.WriteLine("Found the element in BST" + " " + node.RootNode);
+                result = true;
+            }
+            //else
+            //{
+            //    Console.WriteLine("Current element in BST is {0}", node.RootNode);
+            //}
+            if (element.CompareTo(node.RootNode) < 0)
+            {
+                Search(element, node.LeftTree);
+            }
+            if (element.CompareTo(node.RootNode) > 0)
+            {
+                Search(element, node.RightTree);
+            }
+            return result;
+        }
+
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.RootNode.ToString());
+            if (this.RightTree != null)
+            {
+                rightCount++;
+                this.RightTree.Display();
+            }
+        }
+    }
+}
---
+++ MyMapNode.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableandBST.cs
+{
+    public class MyMapNode<K, V>
+    {
+        //this method is for passing Keyvalues in linkedlist, m,n are data types
+        public struct KeyValue<m, n>
+        {
+            public m Key { get; set; }
+            public n Value { get; set; }
+        }
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        //Constructor to initialize 
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+        //Method to find the position of the hash table(creating hash code)
+        protected int GetArrayPosition(K Key)
+        {
+            int hash = Key.GetHashCode();
+            int position = hash % size;
+            return Math.Abs(position);
+        }
+
+        //method to get a value stored in perticular key
+        public V Get(K Key)
+        {
+            int position = GetArrayPosition(Key);
+
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(Key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+
+        //Add method for insert value in hashtable
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>()
+            //assign values to Key and Value
+            { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+
+        //Method to remove a value from hashtable
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+                if (itemFound)
+                {
+                    linkedList.Remove(foundItem);
+                    Console.WriteLine("Removed successfully with key" + foundItem.Key);
+                    break;
+                }
+            }
+        }
+        //method to create a linkedlist,store values in it 
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+
+        //method to display frequencies of word
+        public int CountFrequency(V Value)
+        {
+            int count = 0;
+            foreach (var linkedList in items)
+            {
+                foreach (KeyValue<K, V> item in linkedList)
+                {
+                    if (item.Value.Equals(Value))
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        public void Display()
+        {
+            foreach (var linkedList in items)
+            {
+                if (linkedList != null)
+                    foreach (var element in linkedList)
+                    {
+                        string res = element.ToString();
+                        if (res != null)
+                            Console.WriteLine(element.Key + " " + element.Value);
+                    }
+            }
+        }
+    }
+}
